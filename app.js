1d8b242676990a33cad7e1cebf524a868f2e7300
Sikimic <sikimic94@gmail.com>
2018-05-20 11:58:05 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const boom = require('express-boom');
+const router = express.Router();
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(boom());
+require('./modules')(router);
+app.use(router);
+
+module.exports = app;
---
+++ modules/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var path = require('path');
+var glob = require('glob');
+var env = process.env.NODE_ENV || 'DEBUG';
+
+module.exports = (router) => {
+  glob.sync( './modules/*/index.js' ).forEach( function( file ) {
+    require(path.resolve( file )) (router);
+  });
+};
